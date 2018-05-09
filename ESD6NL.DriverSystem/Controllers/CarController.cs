b9f4d9449a3f39d0f698efc83144985c9d545f85
Violet Kampers <282404@student.fontys.nl>
2018-05-09 16:06:34 +0200

    Changing some stupid mistakes I made, the carOverview and invoiceOverview is accesable

---
+++ ESD6NL.DriverSystem/Controllers/CarController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESD6NL.DriverSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult CarOverview()
+        {
+            return View();
+        }
+    }
+}
---
+++ ESD6NL.DriverSystem/Controllers/InvoiceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESD6NL.DriverSystem.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult InvoiceOverview()
+        {
+            return View();
+        }
+    }
+}
