cfb1bf1489d8bac8596bbbebc1119ca02299bc81
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2018-05-16 08:06:56 +0200

    Authorization also neede for the invoice controller
    Oops xD

--- ESD6NL.DriverSystem/Controllers/InvoiceController.cs
+++ ESD6NL.DriverSystem/Controllers/InvoiceController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ESD6NL.DriverSystem.Controllers
 {
+    [Authorize]
     public class InvoiceController : Controller
     {
         /// <summary>
