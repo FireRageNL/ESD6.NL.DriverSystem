0408cc5bf703b3aeb79616dcd30fd5dba8819a9c
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2018-06-16 08:51:18 +0200

    Added routepoints
    now our rows can have route points, its like magic

--- ESD6NL.DriverSystem.DAL/Migrations/20180616063732_routes.Designer.cs
+++ ESD6NL.DriverSystem.DAL/Migrations/20180616063732_routes.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ESD6NL.DriverSystem.DAL.Migrations
 {
     [DbContext(typeof(DriverSystemContext))]
-    partial class DriverSystemContextModelSnapshot : ModelSnapshot
+    [Migration("20180616063732_routes")]
+    partial class routes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
